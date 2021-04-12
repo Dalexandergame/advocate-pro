fd564d0c8b07dc7f6d0c799ef776ef299160194f
BOUCHKARA AYOUB <76592199+AyoubBouch@users.noreply.github.com>
2021-04-12 12:45:10 +0000

    files names update, files added, first part of view added

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/Authentification', function () {
-    return view('Authentification');
+Route::get('/authentication', function () {
+    return view('authentication');
 });
