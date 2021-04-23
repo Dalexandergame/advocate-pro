fa9ec059009b5eed032349de0cca4c1f10a007f0
M. Bakkas <brahim.bakkas123@gmail.com>
2021-04-23 11:56:52 +0000

    moved the css files to resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,11 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ])
+    .postCss('resources/css/app.css', 'public/css')
+    .postCss('resources/css/welcome.css', 'public/css')
+    .postCss('resources/css/userview.css', 'public/css')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps()
     .postCss('resources/css/sidebar.css', 'public/css/app.css')
     .postCss('resources/css/dashboard.css', 'public/css/app.css')
 ;
