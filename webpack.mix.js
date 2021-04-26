72a823cb5e6645e9e95cf2fbc59d328a996a1215
BOUCHKARA AYOUB <76592199+AyoubBouch@users.noreply.github.com>
2021-04-26 19:07:42 +0000

    edited the auth files to be considered on compile

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,12 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css')
-    .postCss('resources/css/welcome.css', 'public/css')
-    .postCss('resources/css/userview.css', 'public/css')
-    .postCss('resources/css/sidebar.css', 'public/css')
-    .postCss('resources/css/navbar.css', 'public/css')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps()
-;
+mix.js("resources/js/app.js", "public/js")
+    .postCss("resources/css/app.css", "public/css")
+    .postCss("resources/css/welcome.css", "public/css")
+    .postCss("resources/css/userview.css", "public/css")
+    .postCss("resources/css/sidebar.css", "public/css")
+    .postCss("resources/css/navbar.css", "public/css")
+    .postCss("resources/css/auth.css", "public/css")
+    .sass("resources/sass/app.scss", "public/css")
+    .sourceMaps();
