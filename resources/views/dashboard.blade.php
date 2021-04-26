b8f6c5cb06e4d95778ff3350d4ad9c0a05f10680
M. Bakkas <brahim.bakkas123@gmail.com>
2021-04-26 14:23:54 +0000

    here

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,59 +1,6 @@
-<!doctype html>
+@extends('layouts.app')
 
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-
-    <meta charset="utf-8">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-    <!-- CSRF Token -->
-
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-
-    <title>{{ config('app.name', 'AdvocatePRO') }}</title>
-
-
-    <!-- Scripts -->
-
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-
-    <!-- Fonts -->
-
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-
-    <!-- Styles -->
-
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-
-    <style>
-
-        body {
-
-            font-family: 'Nunito', sans-serif;
-        }
-
-    </style>
-</head>
-<body>
-<div class="sidebar">
-    @include('layouts.sidebar')
-</div>
-<div class="navigation">
-    @include('layouts.navbar')
-    <main class="py-4">
-        @yield('content')
-    </main>
-</div>
-<div class="content" style="margin-left: 20%">
+@section('content')
     <div class="container">
         <div class="row">
             <div class="col-5">
@@ -164,5 +111,4 @@
     <div class="taches">
         <img class="w-auto p-5" src="/img/taskspermonth.png"/>
     </div>
-</div>
-</body>
+@endsection
