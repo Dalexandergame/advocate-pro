4c0d466baa036e9b8f3ea5954de93fc1b534e5a8
Abdelhafid El Kadiri <el9adiri@gmail.com>
2021-05-10 16:16:44 +0000

    web.php edited online with Bitbucket

--- routes/web.php
+++ routes/web.php
@@ -65,11 +65,11 @@ Route::get('/taches-details', function () {
     return view('tachesdetails');
 });
 
+Route::resource('templates', TemplatesController::class)->except(['index']);
+
 Route::get('/dossier-juridiques', function () {
     return view('dossierjuridique');
 });
-
-Route::resource('templates', TemplatesController::class)->except(['index']);
 
 Route::get('/jurisprudence', function () {
     return view('jurisprudence');
