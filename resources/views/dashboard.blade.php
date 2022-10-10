b3723d631bc9237c6f77793dc14aeeced6fd3013
Brahim Bakkas <b.brahim@pulse.digital>
2022-10-10 14:18:53 +0100

    fixed Tailwind not working as expected with vite (missing classes)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,17 +13,17 @@
                     <div class="pt-2">Nom d'utilisateur 1</div>
                     <div class="flex justify-between border border-solid border-gray-200 rounded-r-lg bg-gray-200">
                         <div class="bg-black h-6 w-3/5"></div>
-                        <span class="pl-3 h-6 w-2/5">21 Taches</span>
+                        <span class="pl-3 h-6 w-1/5">21 Taches</span>
                     </div>
                     <div class="pt-2">Nom d'utilisateur 1</div>
                     <div class="flex justify-between border border-solid border-gray-200 rounded-r-lg bg-gray-200">
                         <div class="bg-black h-6 w-3/5"></div>
-                        <span class="pl-3 h-6 w-2/5">21 Taches</span>
+                        <span class="pl-3 h-6 w-1/5">21 Taches</span>
                     </div>
                     <div class="pt-2">Nom d'utilisateur 1</div>
                     <div class="flex justify-between border border-solid border-gray-200 rounded-r-lg bg-gray-200">
                         <div class="bg-black h-6 w-3/5"></div>
-                        <span class="pl-3 h-6 w-2/5">21 Taches</span>
+                        <span class="pl-3 h-6 w-1/5">21 Taches</span>
                     </div>
                 </div>
             </div>
@@ -44,7 +44,7 @@
         <div class="flex flex-row gap-x-5 pt-5">
             <div class="basis-1/2">
                 <h5 class="pb-4 font-bold">Taches en cours de fermeture</h5>
-                <div class="shadow-lg bg-white p-4 border border-l-4 border-red-500">
+                <div class="shadow-lg bg-white p-5 border-l-4 border-red-500">
                     <div class="flex justify-between">
                         <div>18072954 (Numero du dossier)</div>
                         <div class="flex">
@@ -68,33 +68,23 @@
             <div class="basis-1/2">
                 <h5 class="pb-4 font-bold">Historique des activités récentes</h5>
                 <div class="bg-white p-4">
-                    <div class="grid grid-cols-3 gap-4">
-                        <div class="">
-                            <h6 class="pb-2 font-bold">Utilisateur</h6>
-                            <div>
+                    <div class="grid grid-cols-7 gap-2">
+                        <span class="col-span-3 pb-2 font-bold">Utilisateur</span>
+                        <span class="col-span-2 pb-2 font-bold">Type d’activité</span>
+                        <span class="col-span-2 pb-2 font-bold">Date et heure</span>
+                    </div>
+                    <div class="grid grid-cols-7 gap-2">
+                        <div class="col-span-3">
+                            <div class="flex items-center space-x-2">
                                 <img src="img/Prof.png" height="22" width="22"/>
-                                <span class="pl-2">Nom d’utilisateur</span><hr>
-                            </div>
-                            <div>
-                                <img src="img/Prof.png" height="22" width="22"/>
-                                <span class="pl-2">Nom d’utilisateur</span><hr>
-                            </div>
-                            <div>
-                                <img src="img/Prof.png" height="22" width="22"/>
-                                <span class="pl-2">Nom d’utilisateur</span><hr>
+                                <span class="pl-2">Nom d’utilisateur</span>
                             </div>
                         </div>
-                        <div class="">
-                            <h6 class="pb-2 font-weight-bold">Type d’activité</h6>
-                            <span>Type d’activité</span><hr>
-                            <span>Type d’activité</span><hr>
-                            <span>Type d’activité</span><hr>
+                        <div class="col-span-2">
+                            <span>Type d’activité</span>
                         </div>
-                        <div class="">
-                            <h6 class="pb-2 font-weight-bold">Date et heure</h6>
-                            <span>Il y a 5h</span><hr>
-                            <span>Il y a 5 min</span><hr>
-                            <span>Il y a 45 min</span><hr>
+                        <div class="col-span-2">
+                            <span>Il y a 5h</span>
                         </div>
                     </div>
                 </div>
@@ -102,7 +92,7 @@
         </div>
     </div>
     <div class="taches">
-        <img class="w-auto p-5" src="img/taskspermonth.png"/>
+        <img class="w-auto py-5" src="img/taskspermonth.png"/>
     </div>
 @endsection
 
